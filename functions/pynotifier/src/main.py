8d5855ecb9b2c6f27c73dd20303762a11fe0d73d
Ryan Shaut <ryan@shaut.us>
2023-11-26 22:33:19 +0000

    comment out unused code

--- functions/pynotifier/src/main.py
+++ functions/pynotifier/src/main.py
@@ -28,24 +28,24 @@ def main(context):
     #     .set_key(os.environ["APPWRITE_API_KEY"])
     # )
 
-    # You can log messages to the console
-    context.log("Hello, Logs!")
+    # # You can log messages to the console
+    # context.log("Hello, Logs!")
 
-    # If something goes wrong, log an error
-    context.error("Hello, Errors!")
+    # # If something goes wrong, log an error
+    # context.error("Hello, Errors!")
 
-    # The `ctx.req` object contains the request data
-    if context.req.method == "GET":
-        # Send a response with the res object helpers
-        # `ctx.res.send()` dispatches a string back to the client
-        return context.res.send("Hello, World!")
+    # # The `ctx.req` object contains the request data
+    # if context.req.method == "GET":
+    #     # Send a response with the res object helpers
+    #     # `ctx.res.send()` dispatches a string back to the client
+    #     return context.res.send("Hello, World!")
 
-    # `ctx.res.json()` is a handy helper for sending JSON
-    return context.res.json(
-        {
-            "motto": "Build like a team of hundreds_",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
-        }
-    )
+    # # `ctx.res.json()` is a handy helper for sending JSON
+    # return context.res.json(
+    #     {
+    #         "motto": "Build like a team of hundreds_",
+    #         "learn": "https://appwrite.io/docs",
+    #         "connect": "https://appwrite.io/discord",
+    #         "getInspired": "https://builtwith.appwrite.io",
+    #     }
+    # )
