3cd33b59aa53683255a764577cc0ce64841e475c
Ryan Shaut <ryan@shaut.us>
2023-11-29 14:26:56 +0000

    Update code

--- functions/pynotifier/src/main.py
+++ functions/pynotifier/src/main.py
@@ -11,6 +11,7 @@ import uuid
 def main(context):
     heartbeatUrl = os.environ.get('REMOTE_HEARTBEAT_URL', None)
     if heartbeatUrl:
+        context.log(f'Calling {heartbeatUrl}')
         res = requests.get(heartbeatUrl)
         context.log(res)
     else:
@@ -24,26 +25,6 @@ def main(context):
             }
         }
 
-    client = (
-        Client()
-        .set_endpoint("https://cloud.appwrite.io/v1")
-        .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-        .set_key(os.environ["APPWRITE_API_KEY"])
-    )
-
-    databases = Databases(client)
-    DATABASE_ID= '6563d535a0e17a3dba0e'
-    COLLECTION = {
-        "ID": 'ef96a122b28c64372c698bee5614ae25',
-        "NAME": "Heartbeats"
-    }
-    # collection = databases.create_collection(DATABASE_ID, COLLECTION['ID'], COLLECTION['NAME'])
-
-    dbDoc = databases.create_document(DATABASE_ID, COLLECTION['ID'], str(uuid.uuid4()), {"data":heartBeatJson})
-
-    return context.res.json(heartBeatJson)
-    # Why not try the Appwrite SDK?
-    #
     # client = (
     #     Client()
     #     .set_endpoint("https://cloud.appwrite.io/v1")
@@ -51,24 +32,18 @@ def main(context):
     #     .set_key(os.environ["APPWRITE_API_KEY"])
     # )
 
-    # # You can log messages to the console
-    # context.log("Hello, Logs!")
+    # databases = Databases(client)
+    # DATABASE_ID= '6563d535a0e17a3dba0e'
+    # COLLECTION = {
+    #     "ID": 'ef96a122b28c64372c698bee5614ae25',
+    #     "NAME": "Heartbeats"
+    # }
+    # # collection = databases.create_collection(DATABASE_ID, COLLECTION['ID'], COLLECTION['NAME'])
 
-    # # If something goes wrong, log an error
-    # context.error("Hello, Errors!")
+    # dbDoc = databases.create_document(DATABASE_ID, COLLECTION['ID'], str(uuid.uuid4()), {"data":heartBeatJson})
 
-    # # The `ctx.req` object contains the request data
-    # if context.req.method == "GET":
-    #     # Send a response with the res object helpers
-    #     # `ctx.res.send()` dispatches a string back to the client
-    #     return context.res.send("Hello, World!")
+    return context.res.json(heartBeatJson)
 
-    # # `ctx.res.json()` is a handy helper for sending JSON
-    # return context.res.json(
-    #     {
-    #         "motto": "Build like a team of hundreds_",
-    #         "learn": "https://appwrite.io/docs",
-    #         "connect": "https://appwrite.io/discord",
-    #         "getInspired": "https://builtwith.appwrite.io",
-    #     }
-    # )
+if __name__ == '__main__':
+    from shared.mockContext import MockContext
+    main(MockContext())
