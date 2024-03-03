74e124c6c3ed6c82e8e4dd5f17c7a1228ffc08b4
Ryan Shaut <ryan@shaut.us>
2024-03-03 00:12:59 +0000

    move shared, add minecraft function

--- functions/minecraft/dataingestor/src/main.py
+++ functions/minecraft/dataingestor/src/main.py
@@ -1,37 +1,27 @@
 from appwrite.client import Client
 import os
+from pyappwriteutils import AppwriteDbClient
 
 
-# This is your Appwrite function
-# It's executed each time we get a request
+MinecraftItemSchema = {
+    "amount": {"type": "string"},
+    "displayName": {"type": "string"},
+    "fingerprint": {"type": "string"},
+    "isCraftable": {"type": "boolean"},
+    "name": {"type": "string"},
+    "tags": {"type": "string"},
+    "lastChangedAt": {"type": "datetime"},
+}
+
 def main(context):
-    # Why not try the Appwrite SDK?
-    #
-    # client = (
-    #     Client()
-    #     .set_endpoint("https://cloud.appwrite.io/v1")
-    #     .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-    #     .set_key(os.environ["APPWRITE_API_KEY"])
-    # )
 
-    # You can log messages to the console
-    context.log("Hello, Logs!")
-
-    # If something goes wrong, log an error
-    context.error("Hello, Errors!")
+    dbClient = AppwriteDbClient(context)
+    db = dbClient.try_create_database("primary_db")
+    collection = dbClient.try_create_collection(db, "Minecraft_items", None, None)
+    # dbDoc = dbClient.addDocument(db, collection, dadJoke)
 
     # The `ctx.req` object contains the request data
     if context.req.method == "GET":
         # Send a response with the res object helpers
         # `ctx.res.send()` dispatches a string back to the client
-        return context.res.send("Hello, World!")
-
-    # `ctx.res.json()` is a handy helper for sending JSON
-    return context.res.json(
-        {
-            "motto": "Build like a team of hundreds_",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
-        }
-    )
+        return context.res.json({"databases":dbClient.get_all_databases()})
