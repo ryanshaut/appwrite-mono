24da102650e6872e48aaca08e3d9a9eb209a2e10
Ryan Shaut <ryan@shaut.us>
2024-03-24 21:47:38 +0000

    add info to response

--- functions/uptime/src/main.js
+++ functions/uptime/src/main.js
@@ -6,7 +6,14 @@ export default async ({ req, res, log, error }) => {
 
   log(new Date());
   if (req.method === 'GET') {
-    return res.json({date: new Date()});
+    return res.json({
+      date: new Date(), 
+      request:{
+        headers: JSON.stringify(req.headers),
+        parameters: JSON.stringify(req.parameters),
+        body: req.body
+      }
+    });
   }
 
 };
