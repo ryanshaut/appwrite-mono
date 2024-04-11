f990e4365ee0977f3b75f53a64886a9416d0be18
Ryan Shaut <ryan@shaut.us>
2024-04-11 18:26:59 -0400

    Dev (#3)
    * add info to response
    
    * add info to response
    
    * add info to response
    
    * add info to response
    
    * add info to response
    
    * add info to response
    
    * add info to response
    
    * add info to response
    
    * add info to response
    
    * update response
    
    * update response
    
    * update response
    
    * update response
    
    * update output
    
    * update output
    
    * update output
    
    * start working on db creation
    
    * remove db connection; update flow
    
    * fix typo
    
    * add mysql connection
    
    * update db logic
    
    * update db logic
    
    * add db access
    
    * fix syntax
    
    * fix typo
    
    * add favicon
    
    * fix typo
    
    * fix logging
    
    * update response
    
    * update response
    
    * fix logging
    
    * update response
    
    * update response
    
    * update response
    
    * update response
    
    * fix logging
    
    * update response
    
    * add sample var
    
    * update for env vars; add version
    
    * clean up code
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * updates
    
    * debug
    
    * fix env vars
    
    * update
    
    * fix response

--- functions/uptime/src/main.js
+++ functions/uptime/src/main.js
@@ -1,12 +1,63 @@
-import { Client } from 'node-appwrite';
+import mysql from 'mysql2/promise';
 
-// This is your Appwrite function
-// It's executed each time we get a request
-export default async ({ req, res, log, error }) => {
+function create_mysql_client(){
 
-  log(new Date());
-  if (req.method === 'GET') {
-    return res.json({date: new Date()});
+  // Create the connection pool. The pool-specific settings are the defaults
+  const pool = mysql.createPool({
+    host: get_env_var('MYSQL_DB_HOST'),
+    user: get_env_var('MYSQL_DB_USER'),
+    database: get_env_var('MYSQL_DB_DATABASE'),
+    password: get_env_var('MYSQL_DB_PASSWORD'),
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
+  });
+  return pool
+}
+
+function get_env_var(key){
+  const value = process.env[key]
+  if (!value){
+    throw new Error(`Missing required env var ${key}`)
   }
+  return value
+
+}
+
+export default async ({ req, res, log, error }) => {
+  // blank favicon request
+  if (req.url === '/favicon.ico') {
+    return res.empty();
+  }
+
+  if (req.query.API_KEY !== get_env_var('UPTIME_CLIENT_API_KEY')){
+    return res.json({error: 'Unauthorized'}, 403)
+  }
+  const db_client = create_mysql_client()
+  
+  const response = {
+    version: '1.0',
+    date: new Date(),
+    source: req.query.source
+  }
+  
+  if (req.query.includeRequest && (req.query.includeRequest == 'true')){
+    response.request = req
+  } 
+  
+  let rows, fields, dbError = null
+  const db_table = get_env_var('MYSQL_DB_TABLE')
+  try {
+    log('Inserting into database')
+    response.dbAction = await db_client.execute(`INSERT INTO ${db_table} (data) VALUES (?)`, [JSON.stringify(response)]);
+  } catch (err) {
+    log(`Error inserting into database: ${err}`);
+    dbError = err;
+  }
+  return res.json({rows, fields, dbError, response});
 
 };
