2c7b6a83a8386da8d1ae9d00460aac36189ef3ea
Ryan Shaut <ryan@shaut.us>
2024-04-11 01:12:23 +0000

    debug

--- functions/uptime/src/main.js
+++ functions/uptime/src/main.js
@@ -51,8 +51,10 @@ export default async ({ req, res, log, error }) => {
   
   let rows, fields, dbError = null
   try {
+    log('Inserting into database')
     table = get_env_var('MYSQL_DB_DATABASE')
     [rows, fields] = await db_client.execute(`INSERT INTO ${table} (data) VALUES (?)`, [JSON.stringify(response)]);
+    log(`Done, inserted ${rows.affectedRows} rows`);
   } catch (err) {
     log(err);
     dbError = err;
